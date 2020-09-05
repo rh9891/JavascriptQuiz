40ce55e4b3f505b8e0cf1802ab6dc55be61d0d91
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-05 09:50:10 -0400

    Coded changes so that the score is pushed to the array, is sorted by value, and then spliced so that the scores display a Top Five.

--- end.js
+++ end.js
@@ -2,13 +2,29 @@ const username = document.getElementById("username");
 const saveScoreButton = document.getElementById("saveScoreButton");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const maxHighScores = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
     saveScoreButton.disabled = !username.value;
-})
+});
 
 saveHighScore = (event) => {
-    console.log("Clicked the save button!")
     event.preventDefault();
-}
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    // Adds the score to the array, sorts it, and then splices out any scores that aren't of the Top Five.
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+};
